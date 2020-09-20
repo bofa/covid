a8c3abec7ec7f2dbb9ad7161ddf378ff96200946
Karlsson Per-Erik <per-erik.karlsson@tieto.com>
2020-09-20 18:03:58 +0200

    Fixed deploy pipeline.

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,16 +3,6 @@ import {
 //   Switch,
 //   Route,
 } from 'react-router-dom';
-// import { Main } from './Main';
-// import GuardedRoute from './GuardedRoute';
-
-import 'normalize.css';
-import '@blueprintjs/core/lib/css/blueprint.css';
-import '@blueprintjs/icons/lib/css/blueprint-icons.css';
-import '@blueprintjs/datetime/lib/css/blueprint-datetime.css';
-import '@blueprintjs/select/lib/css/blueprint-select.css';
-import '@blueprintjs/table/lib/css/table.css';
-// import { AuthenticationProvider, oidcLog } from '@axa-fr/react-oidc-context';
 
 import './App.css';
 import { Main } from './Main';
--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,6 @@ import '@blueprintjs/core/lib/css/blueprint.css';
 import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 import '@blueprintjs/datetime/lib/css/blueprint-datetime.css';
 import '@blueprintjs/select/lib/css/blueprint-select.css';
-import '@blueprintjs/table/lib/css/table.css';
 
 import './App.css';
 import Routes from './Routes';
